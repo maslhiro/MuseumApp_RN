453870f18d1691ff75e35055df44746d22fc8207
Nhut Vinh <nhutvinh14@gmail.com>
2018-10-27 14:43:21 +0700

    Add Firebase Realtime Database :rainbow:
    -Update react-native-firebase ver 5.1.0-rc1  ( fix lỗi fun on và once chỉ chạy 1 lần trong ver 5.0.0 ) :rainbow:

--- android/app/src/main/java/com/twoamteam/museumapp/MainApplication.java
+++ android/app/src/main/java/com/twoamteam/museumapp/MainApplication.java
@@ -5,7 +5,7 @@ import android.app.Application;
 import com.facebook.react.ReactApplication;
 import com.dylanvann.fastimage.FastImageViewPackage;
 import io.invertase.firebase.RNFirebasePackage;
-
+import io.invertase.firebase.database.RNFirebaseDatabasePackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -27,7 +27,8 @@ public class MainApplication extends Application implements ReactApplication {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
           new FastImageViewPackage(),
-          new RNFirebasePackage()
+          new RNFirebasePackage(),
+          new RNFirebaseDatabasePackage()
       );
     }
 
--- src/navigations/HomeSreen/index.js
+++ src/navigations/HomeSreen/index.js
@@ -1,22 +1,55 @@
 import React, { Component } from 'react';
-import { View} from 'react-native';
+import { View, Button} from 'react-native';
 import img_History from '../../assets/CheckBox/img_History.jpg'
 import img_War from '../../assets/CheckBox/img_War.jpg'
 import CustomCheckBox from '../../components/CustomCheckBox'
+import {vinRef} from '../../config/FirebaseConfig'
+console.disableYellowBox = true
 
 export default class HomeScreen extends Component {
   constructor(props) {
     super(props);
     this.state = {
       isCheck: false,
-      isCheck01: false
+      isCheck01: false,
+      data:[]
     };
   }
 
+  componentDidMount(){
+   
+    vinRef.on('value',(childSnapshot)=>{
+      const arr = []
+      childSnapshot.forEach((doc)=>{
+        arr.push({
+          key:doc.key,
+          name:doc.toJSON().Name
+        })  
+    
+      });
+      this.setState(
+        {
+          data : arr
+        }
+      );
+    });
+   
+  }
+
+  tesst = () =>
+  {
+    vinRef.push({
+      ID:'03',
+      Name:"Vinnn"
+    })
+  }
+
+
   render() {
+    console.log("Data",this.state.data)
     return (
       <View style={{flex:1,padding:5,backgroundColor:'white',alignItems:'center',justifyContent:'center'}}>
-        <CustomCheckBox 
+        {/* <CustomCheckBox 
           diameter={180}
           text="Mỹ Thuật"
           image={img_History}
@@ -30,7 +63,9 @@ export default class HomeScreen extends Component {
           image={img_War}
           checked={this.state.isCheck01}
           onPress={()=>{this.setState({isCheck01:!this.state.isCheck01},()=>console.log("OK"))}}
-        />
+        /> */}
+
+        <Button title="Tesst Push Data FireBase" onPress={()=>this.tesst()}/>
 
       </View> 
     );
